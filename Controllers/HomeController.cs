381c577acbebbeeaa544ef37cba8ca22dcfaaeda
ste ste <steelst0@gmail.com>
2018-08-04 21:04:13 -0700

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+using DbConnection;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Process(UserQuotes data)
+        {   
+            string query = $"INSERT INTO userquotes (name, quote) VALUES ('{data.Name}', '{data.Quote}')";
+            DbConnector.Execute(query);
+            ViewData["Message"] = "Quote added to the database!";
+            return RedirectToAction("Index");
+        }
+        
+        [HttpGet("quotes")]
+        public IActionResult Quotes()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
